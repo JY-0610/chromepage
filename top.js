8d35a9273534fe0d17f98db9f98385f674118246
jy-0610 <github.com/jy-0610>
2022-10-13 11:37:50 +0900

    chrome clone page

---
+++ top.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    preventDefaultAnchor();
+  });
+  
+  
+  
+  function preventDefaultAnchor() {
+    $(document).on('click', 'a[href="#"]', function(e) {
+      e.preventDefault();
+    });
+  }
+  
+  
+
+
